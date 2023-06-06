27a5373903ab52e1c9f697f05798469033931ee6
LauraTic <laurac.cardona@udea.edu.com>
2023-06-06 13:20:34 -0500

    con-docuemntacion-url-procesadas

--- SEDENA/contratos-sedena.py
+++ SEDENA/contratos-sedena.py
@@ -29,35 +29,40 @@ def cambiar_nombre(filename):
 
 
 df = pd.read_excel('/Users/datos-lc/Documents/Colecciones ongoing/SEDENA/faltantes.xlsx')
-carpeta_descargas = os.path.expanduser('/Users/datos-lc/Downloads/')
+#ya_procesados = pd.read_csv('/Users/datos-lc/Documents/Colecciones ongoing/SEDENA/2022/procesados.csv')
+carpeta_descargas = '/Users/datos-lc/Downloads/'
+
+#urls_repetidas = df[df["Dirección del anuncio"].isin(ya_procesados["Url ya procesada"])]
+
+# Eliminar URLs repetidas del DataFrame completo
+#url_procesar = df[~df["Dirección del anuncio"].isin(urls_repetidas["Dirección del anuncio"])]
+
+    
+
+url_procesar = df['Dirección del anuncio']
 
 procesados = []
 no_descargados = []  # Lista para almacenar los posibles errores
 descargados = []  # Lista para almacenar los contratos descargados
 
 ### for que usa dos parámetros del dataframe. Para cada una de esas columnas hace las búsquedas y descargas.
-for columna1, columna2 in zip(df['Código del expediente'], df['Dirección del anuncio']):
-   
-    # Número del expediente que aparece en la columna del excel, para comprobar después que sí sea ese el contrato descargado
-    expediente_excel = str(columna1)
-
+for columna2 in url_procesar:
+  
     # Navegar en este enlace
     driver = webdriver.Chrome(columna2)
     driver.get(columna2)
     
     # Obtener el titulo con información del expediente y nombre del contrato que aparece en la página, sirve para nombrar la carpeta con los PDFs
-    titulo = driver.find_element(By.XPATH, "//div[@class='subtitle_01 maintitle']")
-    nombre_carpeta_contrato = titulo.text.strip()
+    nombre_carpeta_contrato = driver.find_element(By.XPATH, "//div[@class='subtitle_01 maintitle']").text().strip()
 
     # Ruta completa de la carpeta para los archivos del contrato
-    ruta_contrato = os.path.join(f'/Users/datos-lc/Documents/Colecciones ongoing/SEDENA/2022/', nombre_carpeta_contrato)
+    ruta_contrato = f'/Users/datos-lc/Documents/Colecciones ongoing/SEDENA/2022/{nombre_carpeta_contrato}/', 
     
 #Si la carpeta con el nombre del contrato existe, ya tiene contenido
 
     if os.path.exists(ruta_contrato):
         
-        print(f"la carpeta{nombre_carpeta_contrato} ya tiene contenido")
-        print(" ------------------------------------------------------")
+        print(f"la carpeta{nombre_carpeta_contrato} ya existe")
         driver.quit()
 #Si no existe no ha sido descargada y procede a crear la carpeta y buscar los pdf para descargarlos.       
     else:
@@ -66,46 +71,50 @@ for columna1, columna2 in zip(df['Código del expediente'], df['Dirección del a
         print(f"la carpeta{nombre_carpeta_contrato} NO EXISTEEEEEEEEEEE") 
         print(" ------------------------------------------------------")
        # Leer el número del expediente que aparece en la página web que estamos navegando
-        aux = driver.find_element(By.XPATH, "//div[@class='form_container'][1]/ul/li/div[@class='form_answer']")
-        expediente_pagina = aux.text.strip()
-
+        expediente_pagina = driver.find_element(By.XPATH, "//div[@class='form_container'][1]/ul/li/div[@class='form_answer']").text.strip()
+        
         #Crear la lista con los elementos descargables
         pdf = driver.find_elements(By.XPATH, "//a[@class='attachTaglink']")
         cantidad_pdf = len(pdf)
-        print(cantidad_pdf)
+        print("Los pdf leídos son: ", cantidad_pdf)
 
 #Cada archivo en pdf se le da click para descargar, si no cargó la página guarda la información que puede y continúa
         for archivo in pdf:
             try:
+                time.sleep(2)
                 archivo.click()
-                time.sleep(11)
+                time.sleep(4)
             except StaleElementReferenceException:
                 print("Elemento obsoleto. Se omitirá y continuará con el siguiente.")
                 print(nombre_carpeta_contrato)
                 print(columna2)
-                no_descargados.append([nombre_carpeta_contrato, cantidad_pdf, expediente_excel, expediente_pagina, columna2])
+                no_descargados.append([nombre_carpeta_contrato, cantidad_pdf, expediente_pagina, columna2])
                 continue
             
 #Hace una lista de los archivos descargados DEBE ESTAR VACIA LA CARPETA DE DESCARGAS ANTES DE INICIAR EL CODIGO
         ruta = '/Users/datos-lc/Downloads/'
         ruta_contrato_edit = ruta_contrato + "/"
-        files = os.listdir('/Users/datos-lc/Downloads/')
+        files = os.listdir(ruta)
+
         try:
-
             if len(files)> 0:
-
                 for descargado in files:
-                    descargado_editado=cambiar_nombre(descargado)
+
+                    descargado_editado = cambiar_nombre(descargado)
                     ruta_vieja = os.path.join(ruta, descargado_editado)
                     ruta_nueva = os.path.join(ruta_contrato_edit, descargado_editado)
+
                     time.sleep(4) 
                     os.rename(ruta_vieja, ruta_nueva)
-                    descargados.append([nombre_carpeta_contrato, cantidad_pdf, expediente_excel, expediente_pagina, descargado, columna2])
+                    descargados.append([nombre_carpeta_contrato, cantidad_pdf, expediente_pagina, descargado, columna2])
                     time.sleep(2)
             else:
                 print("No se encontraron archivos en la carpeta de descargas.")
-                no_descargados.append([nombre_carpeta_contrato, cantidad_pdf, expediente_excel, expediente_pagina])
+                no_descargados.append([nombre_carpeta_contrato, cantidad_pdf, expediente_pagina])
 
+            procesados.append([columna2])
+            df_procesados = pd.DataFrame(procesados, columns=["Url ya procesada"])
+            df_procesados.to_csv('/Users/datos-lc/Documents/Colecciones ongoing/SEDENA/2022/procesados.csv', index=False)
             driver.quit()
         
         except StaleElementReferenceException:
